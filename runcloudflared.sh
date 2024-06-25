687b216a0a56280470610fa4c7d859ab514e5634
skydzhugashvili1 <skyxgamiing+render@gmail.com>
2024-06-25 15:55:45 -0400

    Update runcloudflared.sh

--- runcloudflared.sh
+++ runcloudflared.sh
@@ -1,5 +1,1 @@
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
-
-sudo dpkg -i cloudflared.deb && 
-
-sudo cloudflared service install eyJhIjoiNzQ4MmYzNTIwMTU2OTg0MTA3MTBiYjA2NjljMWQzMWYiLCJ0IjoiNWVkY2JiYTMtYTc5Mi00YzU1LWJkNzgtMjU3MWUwZDU0YWM4IiwicyI6Ik1EY3haR0V4WlRJdE9XVmpNaTAwWldJeUxXSXhNemN0WTJNM016SmtZV0k1WW1VNCJ9
+cd /etc/pterodactyl && sudo wings configure --panel-url https://skyshomepanel.jcbm-mc.uk --token ptla_5tgdiQ8UYhnpitP4JacjpQVk43MfEKlT7fT8NVCrfqR --node 1
