8e4d70940c2fcab1c45fb281cb7b8ff5b97d3ebb
skydzhugashvili1 <skyxgamiing+render@gmail.com>
2024-06-25 16:29:03 -0400

    Update and rename config.yml to sarcfd.sh

---
+++ sarcfd.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiNzQ4MmYzNTIwMTU2OTg0MTA3MTBiYjA2NjljMWQzMWYiLCJ0IjoiOTgwYWI1MGItMzJhOC00MGM4LTk5YmUtMmZhNjMyNGY2MDc2IiwicyI6IllqSTBPVGMxT1RNdE16Sm1NQzAwTkdGbExXSmpObVV0WVdFek5qVXdNRGs1WWpjMSJ9
